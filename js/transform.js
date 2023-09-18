bf2d74ca068ac94cf2ee48e270897f907065ff16
askerovtamerlan <tomcool@yandex.ru>
2023-09-18 21:07:16 +0300

    whazzap

--- js/transform.js
+++ js/transform.js
@@ -1,67 +1,91 @@
 import { NodeIO } from '@gltf-transform/core';
 
-import { reorder, weld, quantize, resample, prune, dedup, draco, center, metalRough, textureCompress } from '@gltf-transform/functions';
+import { reorder, weld, quantize, resample, prune, dedup, draco, center, metalRough } from '@gltf-transform/functions';
 
 import { ALL_EXTENSIONS } from '@gltf-transform/extensions';
 import { KHRONOS_EXTENSIONS } from '@gltf-transform/extensions';
-import { KHRDracoMeshCompression } from '@gltf-transform/extensions';
-import { EXTMeshoptCompression } from '@gltf-transform/extensions';
+// import { KHRTextureBasisu } from '@gltf-transform/extensions';
+// import { KHRDracoMeshCompression } from '@gltf-transform/extensions';
+// import { EXTMeshoptCompression } from '@gltf-transform/extensions';
 
 
 import { MeshoptDecoder, MeshoptEncoder } from 'meshoptimizer';
 import draco3d from 'draco3dgltf';
 
-await MeshoptDecoder.ready;
-await MeshoptEncoder.ready;
+// await MeshoptDecoder.ready;
+// await MeshoptEncoder.ready;
 
 const io = new NodeIO()
     .registerExtensions(ALL_EXTENSIONS)
-    .registerExtensions([EXTMeshoptCompression ])
-    .registerDependencies({
-        'draco3d.decoder': await draco3d.createDecoderModule(), // Optional.
-        'draco3d.encoder': await draco3d.createEncoderModule(), // Optional.
-        'meshopt.decoder': MeshoptDecoder,
-        'meshopt.encoder': MeshoptEncoder,
-    });;
+// .registerExtensions(KHRONOS_EXTENSIONS)
+// .registerExtensions([EXTMeshoptCompression, KHRTextureBasisu])
+// .registerDependencies({
+// 'draco3d.decoder': await draco3d.createDecoderModule(), // Optional.
+// 'draco3d.encoder': await draco3d.createEncoderModule(), // Optional.
+// 'meshopt.decoder': MeshoptDecoder,
+// 'meshopt.encoder': MeshoptEncoder,
+// });;
 
 // Read.
 let document;
-document = await io.read('models/new/Samurai_FixedMet_7.gltf'); // → Document
+document = await io.read('./models/new/KATANA_v3.gltf'); // → Document
 // document = await io.readBinary(glb);   // Uint8Array → Document
+
+
+
 
 // transform 
 
 await document.transform(
     // Losslessly resample animation frames.
-    resample(),
+    // resample(),
     // Remove unused nodes, textures, or other data.
 
-    center(),
+    // center(),
 
-    metalRough(),
+    // metalRough(),
 
     prune(),
+
+    // weld(),
+    // quantize(),
+    // dedup(),
 
     // Remove duplicate vertex or texture data, if any.
     // dedup(),
     // Compress mesh geometry with Draco.
     // draco(),
     // Convert textures to WebP (Requires glTF Transform v3 and Node.js).
-    textureCompress({
-        // encoder: sharp,
-        targetFormat: 'webp',
-        resize: [2048, 2048],
-    }),
+    // textureCompress({
+    // encoder: sharp,
+    // targetFormat: 'ktx2',
+    // resize: [512, 512],
+    // }),
 
-    reorder({ encoder: MeshoptEncoder }),
-    quantize()
+    // reorder({ encoder: MeshoptEncoder }),
+    // quantize({ pattern: /^(POSITION|TEXCOORD|JOINTS|WEIGHTS)(_\d+)?$/ }),
+
+    // draco()
     // Custom transform.
+
+    // backfaceCulling({ cull: true }),
 );
 
-document.createExtension(EXTMeshoptCompression)
-    .setRequired(true)
-    .setEncoderOptions({ method: EXTMeshoptCompression.EncoderMethod.QUANTIZE });
+// function backfaceCulling(options) {
+//     return (document) => {
+//         for (const material of document.getRoot().listMaterials()) {
+//             material.setDoubleSided(!options.cull);
+//         }
+//     };
+// }
+
+// document.createExtension(EXTMeshoptCompression)
+//     .setRequired(true)
+//     .setEncoderOptions({ method: EXTMeshoptCompression.EncoderMethod.FILTER });
+
+// document.createExtension(KHRTextureBasisu)
+//     .setRequired(true);
 
 // Write.
-await io.write('models/new/Samurai_FixedMet_711.glb', document);      // → void
+await io.write('models/new/KATANA_v311.glb', document);      // → void
 // const glb = await io.writeBinary(document); // Document → Uint8Array
